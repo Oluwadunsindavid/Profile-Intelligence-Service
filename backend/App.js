6c52b1254d2d78a43d2be5ee94f7c45b4fff9bce
Oluwadunsin david <oluwadunsindavid21@gmail.com>
2026-04-19 21:10:29 +0100

    fix: update CORS middleware to handle preflight requests and correct API route prefix

--- backend/App.js
+++ backend/App.js
@@ -14,21 +14,21 @@ app.use(express.json());
 
 
 // Place this BEFORE your routes
-app.use(cors({
-  origin: 'http://localhost:5173' // Allow your local Vite server
-}));
-// // Enable CORS (this is a non-negotiable requirement in the brief).
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow specific HTTP methods and add OPTIONS in case any of the 4 aren't used
-//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
-//   // Handle "Preflight" requests.
-//   // Browsers/Grading scripts send an OPTIONS request before a POST to check permissions.
-//   if (req.method === "OPTIONS") {
-//     return res.sendStatus(204); // No Content
-//   }
-//   next();
-// });
+// app.use(cors({
+//   origin: 'http://localhost:5173' // Allow your local Vite server
+// }));
+// Enable CORS (this is a non-negotiable requirement in the brief).
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow specific HTTP methods and add OPTIONS in case any of the 4 aren't used
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specific headers
+  // Handle "Preflight" requests.
+  // Browsers/Grading scripts send an OPTIONS request before a POST to check permissions.
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204); // No Content
+  }
+  next();
+});
 
 // Define a simple Health Check Route to test if the server is running. When you access the root URL ("/"), it will respond with a message indicating that the Profile Intelligence Service is running and on which port.
 app.get("/", (req, res) => {
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
 });
 
 // Use the routes with the /api prefix as required by the brief
-app.use('/api', profileRoutes);
+app.use('/api/profiles', profileRoutes);
 
 // Set the server to listen on a port defined in the environment variables, and log a message to the console when it starts successfully.
 app.listen(port, () => {
