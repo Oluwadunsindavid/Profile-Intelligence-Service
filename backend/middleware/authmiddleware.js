1723d90f22313cdf360fe1e7113bf40d02c89145
Oluwadunsin david <oluwadunsindavid21@gmail.com>
2026-04-20 18:05:59 +0100

    fix: correct casing of userModel import in auth middleware

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/usermodel");
+const User = require("../models/userModel");
 
 const protect = async (req, res, next) => {
   let token;
